42d601d29644e8b42ccc20c0da299bd12860cfa0
cem <cemyilmaztech@gmail.com>
2024-11-23 18:01:45 +0300

    routing

--- main.js
+++ main.js
@@ -24,30 +24,61 @@ const PORT = process.env.PORT || 8000;
 
 
 
-app.get('/', (req, res ,next) => {
-    if(req.query.username == 'Cem'){
-        req.username = 'Cem'
+// app.get('/', (req, res ,next) => {
+//     if(req.query.username == 'Cem'){
+//         req.username = 'Cem'
 
-    res.message = 'Username is Cem'
+//     res.message = 'Username is Cem'
 
-    next();
-    }else {
-        res.send({
-            messsage: 'Error:  Username is wrong'
-        })
-    }
+//     next();
+//     }else {
+//         res.send({
+//             messsage: 'Error:  Username is wrong'
+//         })
+//     }
+// })
+
+// app.get('/', (req, res) => {
+
+//     res.send({
+     
+//  username: req.username,
+//         message: res.message
+//     })
+
+// })
+
+
+
+
+app.get('/', (req, res, next) => {
+req.message1 = 'midlleware-1 started'
+next();
+})
+app.get('/', (req, res, next) => {
+
+    req.message2 = 'middleware-2 started'
+    next()
+    
+})
+app.get('/', (req, res, next) => {
+
+    req.message3 = 'middleware-3 started'
+    next()
+    
 })
 
 app.get('/', (req, res) => {
 
     res.send({
-     
- username: req.username,
-        message: res.message
+        message1: req.message1,
+        message2: req.message2,
+        message3: req.message3,
+       
+       
+        message: 'Finished'
     })
-
 })
-
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
