04fd77240576d75547020a4a9c757e2ae1b4a972
cem <cemyilmaztech@gmail.com>
2024-11-24 18:33:38 +0300

    middleware and routing

--- main.js
+++ main.js
@@ -51,34 +51,62 @@ const PORT = process.env.PORT || 8000;
 
 
 
-app.get('/', (req, res, next) => {
-req.message1 = 'midlleware-1 started'
-next();
-})
-app.get('/', (req, res, next) => {
+// app.get('/', (req, res, next) => {
+// req.message1 = 'midlleware-1 started'
+// next();
+// })
+// app.get('/', (req, res, next) => {
 
-    req.message2 = 'middleware-2 started'
-    next()
+//     req.message2 = 'middleware-2 started'
+//     next()
     
-})
-app.get('/', (req, res, next) => {
+// })
+// app.get('/', (req, res, next) => {
 
-    req.message3 = 'middleware-3 started'
-    next()
+//     req.message3 = 'middleware-3 started'
+//     next()
     
+// })
+
+// app.get('/', (req, res) => {
+
+//     res.send({
+//         message1: req.message1,
+//         message2: req.message2,
+//         message3: req.message3,
+//         message: 'Finished'
+//     })
+// })
+
+const middleFunc1 = (req, res, next) => {
+    console.log('middleFunc1 run');
+    req.message1 = 'middleFunc1 run'
+    next('route')
+}
+
+const middleFunc2 = (req, res, next) => {
+    console.log('middleFunc1 run');
+    req.message2 = 'middleFunc2 run'
+    next()
+}
+
+
+app.get('/home', [middleFunc1, middleFunc2], (req, res) => {
+    res.send({
+        message1: req.message1,
+        message2: req.message2,
+        message : 'finished'
+    })
 })
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
 
     res.send({
         message1: req.message1,
         message2: req.message2,
-        message3: req.message3,
-       
-       
-        message: 'Finished'
+        message: 'next.Route - Finished'
     })
+    
 })
-
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
